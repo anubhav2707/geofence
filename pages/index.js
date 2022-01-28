44d5fb9f0c91d88095f63b366bd0f2172be8c333
Anubhav27 <anubhav022700@gmail.com>
2022-01-28 21:56:24 +0530

    Added location state access

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { geolocated } from "react-geolocated";
 
-export default function Home() {
-  return (
+const Home = (props) => {
+  return !props.isGeolocationEnabled ? (
     <div className={styles.container}>
       <Head>
         <title>GeoFence</title>
@@ -14,16 +14,48 @@ export default function Home() {
           Welcome to <b>GeoFence</b>
         </h1>
 
-        <p className={styles.description}>
-          The app needs your permission to use your location. Get started by allowing location access.
+        <p className={styles.description, styles.redText}>
+          Please allow location access to continue.
         </p>
-
-      <div className='btn-container'>
+        {/* <div className='btn-container'>
         <button>Allow Access</button>
-      </div>
+        </div> */}
+      </main>
+
+    </div>
+  ) :
+  (
+    <div className={styles.container}>
+      <Head>
+        <title>GeoFence</title>
+      </Head>
+
+      {console.log(props)}
+
+      <main className={styles.main}>
+        <h2>Location access granted!</h2>
+        <table>
+                <tbody>
+                    <tr>
+                        <td>latitude</td>
+                        <td>{props.coords?.latitude}</td>
+                    </tr>
+                    <tr>
+                        <td>longitude</td>
+                        <td>{props.coords?.longitude}</td>
+                    </tr>
+                </tbody>
+            </table>
 
       </main>
 
     </div>
   )
 }
+
+export default geolocated({
+  positionOptions: {
+      enableHighAccuracy: true,
+  },
+  userDecisionTimeout: 100000,
+})(Home);
